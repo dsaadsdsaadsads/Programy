da50d01a050aebf661ef82da637dae45ef2381c0
Patryk Panek <ppanek327@technikumkreatywne.pl>
2022-04-08 13:41:45 +0200

    nwd i nww:

---
+++ nwd_nww.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a;
+    int b;
+    cout << "Podaj liczbe a: ";
+    cin >> a;
+    cout << "Podaj liczbe b: ";
+    cin >> b;
+
+    int x = a * b;
+
+    while (a != b)
+    {
+        if (a < b) {
+            b = b - a;
+        }
+        else {
+             a = a - b;
+        }    
+    }
+    cout << "NWD: ";
+    cout << a;
+    int nwd = x / a;
+    cout << "NWW: ";
+    cout << nwd;
+}
