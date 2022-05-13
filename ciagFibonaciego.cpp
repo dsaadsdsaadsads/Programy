394c7fce3ede6c0a718e1075cea8791a5faab4e9
Patryk Panek <ppanek327@technikumkreatywne.pl>
2022-05-13 13:16:53 +0200

    Ciag Fibonaciego

---
+++ ciagFibonaciego.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int f0 = 0;
+    int f1 = 1;
+    int f;
+    cout << "Podaj liczbę wyrazów ciągu do wpisania: ";
+    cin >> n;
+    for (int i = 0; i <= n; i++) {
+        if (i > 1) {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+        }
+        else {
+            f = i;
+        }
+        cout << f << endl;
+    }
+}
+
